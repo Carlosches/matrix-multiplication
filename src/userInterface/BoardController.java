68e61d834d2b2f4fb6b31c763016ff4b2ed768ed
Felipe Sanchez <vegafelipe015@gmail.com>
2019-08-24 09:14:50 -0500

    one exception catch

--- src/userInterface/BoardController.java
+++ src/userInterface/BoardController.java
@@ -1,7 +1,13 @@
 package userInterface;
 
+import java.awt.font.NumericShaper;
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
@@ -9,6 +15,7 @@ import javafx.scene.control.Tab;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 public class BoardController {
 
@@ -51,41 +58,72 @@ public class BoardController {
     @FXML
     void tabOneGenerateButton(ActionEvent event) {
     	
-    	int rows = Integer.parseInt(rowMatrixOne.getText());
-    	int columns = Integer.parseInt(columnMatrixOne.getText());
+    	try {
+    		
+    		tabOneLabel.setText("");
     	
-    	GridPane gridPane = new GridPane();
-    	scrollPaneOneA.setContent(gridPane);
+    		int rows = Integer.parseInt(rowMatrixOne.getText());
+    		int columns = Integer.parseInt(columnMatrixOne.getText());
+    		
+    		GridPane gridPane = new GridPane();
+        	scrollPaneOneA.setContent(gridPane);
+        	
+        	gridPane.getChildren().clear();
+        	
+        	 
+        	for (int i = 0; i < columns; i++) {
+    			for (int j = 0; j < rows; j++) {
+    				gridPane.add(new Button(" F "), i, j);
+    			}
+    		}
+        	
+        	int rowsB = Integer.parseInt(rowMatrixTwo.getText());
+        	int columnsB = Integer.parseInt(columnMatrixTwo.getText());
+        	
+        	GridPane gridPane2 = new GridPane();
+        	scrollPaneOneB.setContent(gridPane2);
+        	
+        	gridPane2.getChildren().clear();
+        	
+        	 
+        	for (int i = 0; i < columnsB; i++) {
+    			for (int j = 0; j < rowsB; j++) {
+    				gridPane2.add(new Button(" F "), i, j);
+    			}
+    		}
     	
-    	gridPane.getChildren().clear();
-    	
-    	 
-    	for (int i = 0; i < columns; i++) {
-			for (int j = 0; j < rows; j++) {
-				gridPane.add(new Button(" F "), i, j);
-			}
-		}
-    	
-    	int rowsB = Integer.parseInt(rowMatrixTwo.getText());
-    	int columnsB = Integer.parseInt(columnMatrixTwo.getText());
-    	
-    	GridPane gridPane2 = new GridPane();
-    	scrollPaneOneB.setContent(gridPane2);
-    	
-    	gridPane2.getChildren().clear();
-    	
-    	 
-    	for (int i = 0; i < columnsB; i++) {
-			for (int j = 0; j < rowsB; j++) {
-				gridPane2.add(new Button(" F "), i, j);
-			}
-		}
+    	}catch(NumberFormatException e) {
+    		
+    		tabOneLabel.setText("PLEASE, INTRODUCE ONLY INTEGERS");
+    		
+    	}
 
     }
 
     @FXML
     void tabOneMultiplyButton(ActionEvent event) {
-
+    	
+    	
+		try {
+			
+			FXMLLoader fxmlLoader= new FXMLLoader(getClass().getResource("outcome.fxml"));
+			Parent root1= (Parent)fxmlLoader.load();
+			
+			Stage stage= new Stage();
+			
+			stage.setTitle("OUTCOME MATRIX");
+			
+			stage.setScene(new Scene(root1));
+			
+			stage.show();
+			
+		} catch (IOException e) {
+			
+			System.out.println("ERROR");
+			e.printStackTrace();
+			
+		}
+    	
     }
 
     @FXML
---
+++ src/userInterface/OutcomeController.java
@@ -0,0 +1,12 @@
+package userInterface;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+
+public class OutcomeController {
+
+    @FXML
+    private ScrollPane outcomeScrollPane;
+
+}
+
